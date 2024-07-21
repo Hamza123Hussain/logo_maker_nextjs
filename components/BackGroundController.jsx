add5357a19b161856212d11e3275670bf5e224a9
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-07-21 06:07:11 +0500

    Modifying width of the background

--- components/BackGroundController.jsx
+++ components/BackGroundController.jsx
@@ -5,51 +5,50 @@ import ColourPicker from './ColourPickerController'
 import { ValuesContext } from '@/context/Context'
 
 const BackgroundController = () => {
-  const [IconValues, SetIconValues] = useContext(ValuesContext)
-  const isBrowser = typeof window !== 'undefined' // this helps in removing the undefined local storage error when deploying
-  const StoredValue = isBrowser
-    ? JSON.parse(localStorage.getItem('Values'))
-    : null
+  const { backgroundValue, setBackgroundValue } = useContext(ValuesContext)
 
-  const [Padding, setPadding] = useState(
-    StoredValue ? StoredValue?.BG_Padding : 28
-  )
-  const [Round, setRound] = useState(StoredValue ? StoredValue?.BG_Round : 0)
-  const [color, setcolor] = useState(
-    StoredValue ? StoredValue?.BG_COLOR : 'rgba(255,255,255,1)'
-  )
+  const StoredValue = JSON.parse(localStorage.getItem('BG'))
+  const [Width, setWidth] = useState(0)
+  const [Round, setRound] = useState(0)
+  const [color, setcolor] = useState('rgba(255,255,255,1)')
+  useEffect(() => {
+    if (StoredValue) {
+      setRound(StoredValue.Round || 28)
+      setWidth(StoredValue.Width || 0)
+      setcolor(StoredValue.color || 'rgba(255,255,255,1)')
+      setBackgroundValue(StoredValue)
+    }
+  }, [])
 
   useEffect(() => {
-    if (isBrowser) {
-      const BGValues = {
-        ...StoredValue,
-        BG_Padding: Padding,
-        BG_Round: Round,
-        BG_COLOR: color,
-      }
-      SetIconValues(BGValues)
-      localStorage.setItem('Values', JSON.stringify(BGValues))
-    }
-  }, [color, Padding, Round])
+    setBackgroundValue((Element) => ({
+      ...Element,
+      color: color,
+      Width: Width,
+      Round: Round,
+    }))
 
-  console.log(IconValues)
+    localStorage.setItem('BG', JSON.stringify(backgroundValue))
+  }, [color, Width, Round])
+
+  // console.log(IconValues)
 
   return (
     <div className="flex flex-col mx-3 gap-2 my-5">
       <div className="flex flex-col">
-        {/* <div className="flex justify-between">
-          <p>Padding</p>
+        <div className="flex justify-between">
+          <p>Width</p>
           <p>
-            {Padding}
+            {Width}
             px
           </p>
         </div>
         <input
           type="range"
-          value={Padding}
-          max={200}
-          onChange={(e) => setPadding(Number(e.target.value))}
-        /> */}
+          value={Width}
+          max={600}
+          onChange={(e) => setWidth(Number(e.target.value))}
+        />
       </div>
       <div className="flex flex-col">
         <div className="flex justify-between">
